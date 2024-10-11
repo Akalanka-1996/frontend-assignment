3649ce3fd7841bc3ad55a943cda8aca1114120a4
kasun <kasunakalanka4@gmail.com>
2024-10-11 14:05:04 +0530

    fix axios issue

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import type { AxiosError } from "axios";
 import axios from "axios";
 
 import type { CommonResponseType } from "@/types/common";
@@ -18,7 +17,7 @@ const axiosService = () => {
     const session = await getSession();
 
     if (session) {
-      request.headers.Authorization = `Bearer ${session.user.token}`;
+      request.headers.Authorization = `Bearer ${session.user.token}`
     }
 
     return request;
@@ -29,37 +28,6 @@ const axiosService = () => {
       return response;
     },
     async (error) => {
-      const originalRequest = error.config;
-
-      if (error.response?.status === 403 && !originalRequest._retry) {
-        originalRequest._retry = true;
-
-        try {
-          const session = await getSession();
-
-          const refreshResponse = await axios.post(
-            `${process.env.BASE_URL}/api/refresh`,
-            {
-              refreshToken: session?.user.refreshToken,
-            }
-          );
-
-          const { access_token } = refreshResponse.data.data;
-
-          originalRequest.headers.Authorization = `Bearer ${access_token}`;
-          return axios(originalRequest);
-        } catch (refreshError) {
-          console.log(refreshError);
-          return Promise.reject({
-            status: "FAIL",
-            message: "Failed to refresh token",
-            data: null,
-          });
-        }
-      }
-
-      console.log((error as AxiosError).response?.data);
-
       return Promise.reject(error.response?.data as CommonResponseType);
     }
   );
